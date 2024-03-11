752728126bf0ca73fe7c7156c400d8d58ca82cb9
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2024-03-11 18:48:22 +0100

    checkpoint

--- app/src/main/java/com/algorandfoundation/arc52_android_wallet/FirstFragment.kt
+++ app/src/main/java/com/algorandfoundation/arc52_android_wallet/FirstFragment.kt
@@ -2,38 +2,47 @@ package com.algorandfoundation.arc52_android_wallet
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import bip32ed25519.Bip32Ed25519
+import bip32ed25519.KeyContext
+import cash.z.ecc.android.bip39.Mnemonics.MnemonicCode
+import cash.z.ecc.android.bip39.toSeed
 import com.algorandfoundation.arc52_android_wallet.databinding.FragmentFirstBinding
-
 import com.goterl.lazysodium.LazySodiumAndroid
 import com.goterl.lazysodium.SodiumAndroid
+import com.goterl.lazysodium.utils.HexMessageEncoder
+import java.nio.charset.StandardCharsets
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+/** A simple [Fragment] subclass as the default destination in the navigation. */
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding
+        get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
 
-        val ls = LazySodiumAndroid(SodiumAndroid())
-        val p = ls.cryptoSignKeypair()
-        Log.d("sk key", p.secretKey.asHexString)
-        Log.d("pk key", p.publicKey.asHexString)
+        val seed =
+                MnemonicCode(
+                        "salon zoo engage submit smile frost later decide wing sight chaos renew lizard rely canal coral scene hobby scare step bus leaf tobacco slice".toCharArray()
+                )
+        val ls =
+                LazySodiumAndroid(SodiumAndroid("sodium"))
+        val c = Bip32Ed25519(ls, seed.toSeed())
+        val pk = c.keyGen(KeyContext.Address, 0u, 0u, 0u)
+        Log.d("public key:", pk.toString())
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
